7f78b5fb23defe3e9bb5995534ceb85d68fc1683
Ramon <rsalado7729@gmail.com>
2018-12-18 17:53:43 -0200

    Remove ouput zipmap

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,6 @@ output "kms_key_arn" {
   value = "${aws_kms_key.this.arn}"
 }
 
-output "map_name_key_id" {
-  value = "${zipmap(aws_kms_alias.this.*.name,aws_kms_key.this.*.id)}"
-}
-
-output "map_name_key_arn" {
-  value = "${zipmap(aws_kms_alias.this.*.name,aws_kms_key.this.*.arn)}"
-}
-
 output "alias_name" {
   value = "${aws_kms_alias.this.name}"
 }
